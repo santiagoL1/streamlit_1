c1ab80a411a1598552c3a4ff3a72a19764b07412
santiagoL1 <122912749+santiagoL1@users.noreply.github.com>
2024-09-26 11:18:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
 import streamlit as st 
+st.title('My first streamlit app')
+st.write('This is a simple streamlit app')
+user_input = st.text_input("enter your name")
+st.write('your name is ', user_input)
+age = st.slider("choose your age",1,100)
+st.write("you are ", age, " years old")
+if st.button('say hello'):
+  st.write("hello there")
+
